e5c000546ad739747949a8fcaff029d2d1a56183
Josef Ohnheiser <ritero@ritero.cz>
2014-09-12 08:34:21 +0200

    Helpres and Request class

---
+++ src/ritero/SDK/TwitchTV/Helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ritero\SDK\TwitchTV;
+
+/**
+ * TwitchException for TwitchTV API SDK for PHP
+ *
+ * PHP SDK for interacting with the TwitchTV API
+ *
+ * @author Josef Ohnheiser <ritero@ritero.eu>
+ * @license https://github.com/jofner/Twitch-SDK/blob/master/LICENSE.md MIT
+ * @homepage https://github.com/jofner/Twitch-SDK
+ */
+class Helpers {
+
+}
