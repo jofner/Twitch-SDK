3b0f599d66994b747986d3ad866b6ca77838aa10
Josef Ohnheiser <ritero@ritero.cz>
2014-09-12 08:31:34 +0200

    Classes for methods separation

---
+++ src/ritero/SDK/TwitchTV/Methods/Teams.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ritero\SDK\TwitchTV\Methods;
+
+/**
+ * TwitchTV API SDK for PHP
+ *
+ * Teams method class
+ *
+ * @author Josef Ohnheiser <ritero@ritero.eu>
+ * @license https://github.com/jofner/Twitch-SDK/blob/master/LICENSE.md MIT
+ * @homepage https://github.com/jofner/Twitch-SDK
+ */
+class Teams {
+
+}
