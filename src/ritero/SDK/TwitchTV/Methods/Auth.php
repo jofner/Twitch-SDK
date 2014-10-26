2bd32aff3100316a3104f684aa708119739cb77b
Josef Ohnheiser <ritero@ritero.cz>
2014-10-26 17:58:10 +0100

    Authenticate method class

---
+++ src/ritero/SDK/TwitchTV/Methods/Auth.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ritero\SDK\TwitchTV\Methods;
+
+/**
+ * TwitchTV API SDK for PHP
+ *
+ * Authenticate method class
+ *
+ * @author Josef Ohnheiser <ritero@ritero.eu>
+ * @license https://github.com/jofner/Twitch-SDK/blob/master/LICENSE.md MIT
+ * @homepage https://github.com/jofner/Twitch-SDK
+ */
+
+class Auth
+{
+
+}
